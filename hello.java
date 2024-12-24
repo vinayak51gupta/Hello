d9aa9753716381cb9c603f999e0b039fdf5aa219
vinayak51gupta <vinayak51gupta@gmail.com>
2024-12-25 00:28:09 +0530

    fixed bugs

---
+++ hello.java
@@ -0,0 +1,12 @@
+package Hello;
+
+public class hello {
+    public static void main(String args[]){
+     Bankacc b1= new Bankacc();
+     b1.name = "Vinayak";
+     System.out.println(b1.name);
+    }
+}
+class Bankacc{
+    String name;
+}
